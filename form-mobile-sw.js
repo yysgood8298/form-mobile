92a49a63b7c28459f1c08051c8e0eaf1e30cf812
yysgood8298 <yysgood8298@gmail.com>
2026-08-13 13:24:26 +0900

    Deploy FORM mobile assessment app

---
+++ form-mobile-sw.js
@@ -0,0 +1,1 @@
+const CACHE='form-mobile-v1';const ASSETS=['./form-mobile.html','./form-mobile.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
